f1ab97c08c89fb4b9cd71c3962a2a3ed8e6c4fec
Roger Shyaka <roshyak@gmail.com>
2025-05-22 21:00:33 -0400

    react routing

---
+++ src/components/PokemonForm/PokemonForm.jsx
@@ -0,0 +1,68 @@
+// src/components/PokemonForm/PokemonForm.jsx
+import { useNavigate } from 'react-router';
+import { useState } from 'react';
+
+const initialState = {
+  name: '',
+  weight: 0,
+  height: 0,
+};
+
+const PokemonForm = (props) => {
+  const [formData, setFormData] = useState(initialState);
+  
+  const navigate = useNavigate();
+
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    // update the form data
+    props.addPokemon(formData);
+
+    //reseting the form
+    setFormData(initialState);
+
+    //sending the user to the list of pokemon 
+    navigate('/pokemon');
+  };
+
+  const handleChange = ({ target }) => {
+    setFormData({ ...formData, [target.name]: target.value });
+  };
+
+  return (
+    <main>
+      <h2>New Pokemon</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="weight">Weight:</label>
+        <input
+          type="number"
+          id="weight"
+          name="weight"
+          value={formData.weight}
+          onChange={handleChange}
+        />
+        <label htmlFor="height">Height:</label>
+        <input
+          type="number"
+          id="height"
+          name="height"
+          value={formData.height}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
+
+export default PokemonForm;
